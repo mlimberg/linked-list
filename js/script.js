8f581f67d6276a98f3defc4e9130ae52e4da2d61
mlimberg <limbergmike@gmail.com>
2016-10-19 08:55:25 -0600

    add js to clear fields

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@ var $link = $('.website-link-input');
 var $enter = $('.enter');
 var $bookmarkTitle = $('.bookmark-title');
 var $bookmarkLink = $('.bookmark-link');
+var $bookmarkSection = $('.bookmark-section');
 
 function Bookmark(title, link) {
   this.title = $title.val();
@@ -11,18 +12,26 @@ function Bookmark(title, link) {
 
 function addBookmark() {
     var currentBookmark = new Bookmark();
-    if (currentBookmark['title'] === "" && currentBookmark['link'] === "") {
+    if (currentBookmark['title'] === "" || currentBookmark['link'] === "") {
       alert('You must enter a title and a URL!');
   } else {
-    $bookmarkTitle.append("Title: " + currentBookmark['title']);
-    $bookmarkLink.append("URL: " + '<a href="' + currentBookmark['link'] + '" target="_blank">Some Text</a>');
+    $bookmarkTitle.append(currentBookmark['title']);
+    $bookmarkLink.append('<a href=' + currentBookmark['link'] + '" target="_blank">'+ currentBookmark['link'] + '</a>');
   }
 }
 
+function toggleBookmark() {
+  $bookmarkSection.addClass('new-bookmark');
+}
 
-
+function clearFields() {
+  $title.val("");
+  $link.val("");
+}
 $enter.on('click', function() {
+      toggleBookmark();
       addBookmark();
+      clearFields();
   });
 
 // ! Bookmark only works if user enters "http://"
