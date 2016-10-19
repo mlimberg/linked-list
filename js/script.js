8bbe60139ee8e6cd7d9464956c382d28dc7b3f45
mlimberg <limbergmike@gmail.com>
2016-10-18 18:48:57 -0600

    wrote first js function

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
+var $title = $('.website-title-input');
+var $link = $('.website-link-input');
+var $enter = $('.enter');
 
+function Bookmark(title, link) {
+  this.title = title;
+  this.link = link;
+}
+
+
+
+
+$('.enter').on('click', function() {
+      console.log($title.val());
+  });
