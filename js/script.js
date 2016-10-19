769708ba2b03aa8b29dba43f9ef2627fb5b8122d
Dylan Avery <fossilfuelmusic@gmail.com>
2016-10-19 13:34:59 -0600

    Dedecorated all focusies

--- js/script.js
+++ js/script.js
@@ -5,29 +5,24 @@ var $bookmarkTitle = $('.bookmark-title');
 var $bookmarkLink = $('.bookmark-link');
 var $bookmarkSection = $('.bookmark-section');
 
+
 function Bookmark(title, link) {
   this.title = $title.val();
   this.link = $link.val();
 }
 
-
-// function toggleBookmark() {
-//   $bookmarkSection.addClass('new-bookmark');
-// }
-
 function clearFields() {
   $title.val("");
   $link.val("");
 }
+
 $enter.on('click', function() {
       newSection();
-      // toggleBookmark();
       clearFields();
   });
 
   $('input').keypress(function(event) {
      if (event.which === 13) {
-      //  toggleBookmark();
        newSection();
        clearFields();
      }
@@ -39,9 +34,12 @@ function newSection() {
 var currentBookmark = new Bookmark();
 var listItem = currentBookmark['title'];
 var listLink = currentBookmark['link'];
+var titles = ('<div class="new-bookmark">' + '<div class="new-title">' + listItem + '</div>' + '<div class="new-link">');
+var links = ('<a href="' + currentBookmark['link'] + '" target="_blank">'+ currentBookmark['link'] + '</a>'+ '</div>');
+var buttons = ('<div class="button-block">' + '<button type="button" name="button" id="marked">Read</button><button type="button" name="button" id="remove">Delete</button>' + '</div>' + '</div>');
 if (currentBookmark['title'] === "" || currentBookmark['link'] === "") {
   alert('You must enter a title and a URL!');
        }  else {
-$bookmarkSection.prepend('<div class="new-bookmark">' + '<div class="new-title">' + listItem + '</div>' + '<div class="new-link">' + '<a href="' + currentBookmark['link'] + '" target="_blank">'+ currentBookmark['link'] + '</a>'+ '</div>' + '</div>');
+$bookmarkSection.prepend(titles + links + buttons);
        }
    }
